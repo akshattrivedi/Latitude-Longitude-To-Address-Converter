bc9b3981b56fa9cd28773ca19a6e485dfc46562d
akshattrivedi <akshat.trivedi08@gmail.com>
2021-05-16 04:06:25 +0530

    fix: GeoLocator's Exception Handling

--- app/latLongToAddressConverter.py
+++ app/latLongToAddressConverter.py
@@ -1,28 +1,26 @@
 from geopy.geocoders import Nominatim #for getting location from latitute and longitude
 
 # initialisation
+class AddressConverter:
+    def __init__(self):
+        pass
 
-def findAddress(lat, lon):
-    geolocator = Nominatim(user_agent="App",timeout=100)
+    def findAddress(self, lat, lon):
+        geolocator = Nominatim(user_agent="App",timeout=100)
+        location = None
 
-    if not (isFloat(lat) and isFloat(lon)):
-        return "Incorrect Co-ordinates Input"
+        s = str(lat) + "," + str(lon)
 
-    s = str(lat) + "," + str(lon)
+        # Getting Street Address of the Location
+        try:
+            location = geolocator.reverse(s)
+        except ValueError as e:
+            return e
 
-    # Getting Street Address of the Location
-    location = geolocator.reverse(s)
+        if location == None:
+            return "Location's Street Address Not Found!"
+        else:
+            strt = location.address
+            return strt
 
-    if location == None:
-        return "Location's Street Address Not Found!"
-    else:
-        strt = location.address
-        return strt
-
-def isFloat(value: str):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
 
